a788cec3f1e665beee88e3d74e9b40d81482cdb2
James Jones <jamjon3@gmail.com>
2012-08-17 11:34:17 -0400

    Added a file count for the history and voter imports. The order of import is not alphabetical and the count lets you know the file number being parsed (67 for each import loop)

--- Import/Import.php
+++ Import/Import.php
@@ -1,14 +1,27 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Import
+ * Providing for the import of raw text file interation for importing into the database
+ * 
+ * This handles iterating over directories containing voter and history files and passing those files into the voter and history parsers
+ * 
+ * CONTACT: jamjon3@stpetegreens.org
+ * 
+ * LICENSE: This source file is free software, under the GPL v2 license, as supplied with this software.
  *
- * @author jam
+ * This source file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
+ *
+ * For details please refer to: https://stpetgreens.org
+ * 
+ * @package     Import.php
+ * @version     0.1
+ * @author      James Jones (stpetegreens.org)
+ * @since       04/11/2012
+ * @license     GPL v2
+ * @category    FloridaVotersManager
+ * @copyright   2011-2012 James Jones, all rights reserved.
  */
 include_once 'County.php';
 include_once 'Voters.php';
@@ -22,13 +35,15 @@ class Import extends County {
     }
     
     function importVoters() {
+        $fileCount = 0;
         $importPath = getcwd()."/../Voters/";
         if ($handle = opendir($importPath)) {
             /* This is the correct way to loop over the directory. */
             while (false !== ($filename = readdir($handle))) {
                 if ($filename != "." && $filename != ".." && $filename != ".htaccess") {
                     if(!is_dir($importPath."/".$filename)) {
-                        print $filename."\n";
+                        ++$fileCount;
+                        print "Parsing ".$filename." file count of ".(++$fileCount)."\n";
                         new Voters($filename);
                     }                    
                 }                
@@ -36,13 +51,14 @@ class Import extends County {
         }        
     }
     function importHistories() {
+        $fileCount = 0;
         $importPath = getcwd()."/../Histories/";
         if ($handle = opendir($importPath)) {
             /* This is the correct way to loop over the directory. */
             while (false !== ($filename = readdir($handle))) {
                 if ($filename != "." && $filename != ".." && $filename != ".htaccess") {
                     if(!is_dir($importPath."/".$filename)) {
-                        print $filename."\n";
+                        print "Parsing ".$filename." file count of ".(++$fileCount)."\n";
                         new Histories($filename);
                     }                    
                 }                
